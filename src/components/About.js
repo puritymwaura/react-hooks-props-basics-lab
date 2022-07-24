61ad5b33583f8835690d6272454770866dc628e6
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-07-24 18:18:18 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,29 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links";
 
-function About() {
-  return (
-    <div id="about">
+
+function About(props) {
+
+  // const bioTrue=props.bio;
+  // const size=((props.bio).length)
+  // if((bioTrue)&&size>1){
+
+
+    
+    return (
+      <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+   
+     {props.bio && props.bio.length>0?<p>{props.bio}</p>:null}
+     
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      
+       <Links github= {props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
+
+
 
 export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,12 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links}/>
     </div>
   );
 }
 
 export default App;
+
+
